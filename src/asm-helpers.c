9b7771414594861fcad3e91f824a3adc6e9f9923
Pablo <pablo.inoriza@despegar.com>
2019-09-30 01:00:53 -0300

    Adding assembly code for jumps

--- src/asm-helpers.c
+++ src/asm-helpers.c
@@ -1,7 +1,19 @@
 #include "builders.h"
+#include "ant_engine.h"
 
-void 
-doPanic(char *s)
-{
+void doPanic(char *s) {
   panic(s);
+}
+
+void doPanicd(char *s, orientation_t orientation) {
+  //panicd(s, orientation);
+}
+
+void debug(char *s)
+{
+  printf("%s\n", s);
+}
+
+void doAdjust(uint32_t *x, uint32_t y, orientation_t orientation) {
+  adjust(x, y, orientation);
 }
