b0831075742a4ea373831d2eded7255e5b29de30
Pablo <pablo.inoriza@despegar.com>
2019-10-03 19:56:58 -0300

    The code is already working

--- src/asm-helpers.c
+++ src/asm-helpers.c
@@ -5,15 +5,11 @@ void doPanic(char *s) {
   panic(s);
 }
 
-void doPanicd(char *s, orientation_t orientation) {
-  //panicd(s, orientation);
-}
-
 void debug(char *s)
 {
   printf("%s\n", s);
 }
 
-void doAdjust(uint32_t *x, uint32_t y, orientation_t orientation) {
-  adjust(x, y, orientation);
+void doAdjust(uint32_t *prevPos, uint32_t newPos, uint32_t limit) {
+  adjust(prevPos, newPos, limit);
 }
